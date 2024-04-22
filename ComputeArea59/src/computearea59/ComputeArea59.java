9760b61a12758721695e4a623b3d4bace3f7baa6
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-04-23 04:46:29 +0600

    Home
    hw

--- ComputeArea59/src/computearea59/ComputeArea59.java
+++ ComputeArea59/src/computearea59/ComputeArea59.java
@@ -1,17 +1,19 @@
-
 package computearea59;
+
+import java.util.Scanner;
 
 public class ComputeArea59 {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Pls enter a number");
         int redious;
-        redious =10;
+        redious = input.nextInt();
         double PI = 3.1416;
-        
-        double area = redious*redious*PI;
+
+        double area = redious * redious * PI;
         System.out.println(area);
-        
+
     }
 
 }
