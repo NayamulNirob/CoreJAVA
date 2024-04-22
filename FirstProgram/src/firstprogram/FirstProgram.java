0392c303b0a9037d491cd18548b41d8c4bf51241
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-04-23 01:35:43 +0600

    H
    hh

--- FirstProgram/src/firstprogram/FirstProgram.java
+++ FirstProgram/src/firstprogram/FirstProgram.java
@@ -16,43 +16,37 @@ public class FirstProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-       
-        
+
         Scanner input = new Scanner(System.in);
-        
-//       System.out.println("Pls enter the value of Radius");
-//        int radius = input.nextInt();
-//        double area;
-//        area = radius*radius*3.1416;
-//        System.out.println(area);
-        
-//        System.out.println("Please enter height");
-//        float height = input.nextFloat();
-//        System.out.println("Please enter wedth");
-//          float  wedht = input.nextFloat();
-//        double area;
-//        area = height*wedht;
-//        System.out.println( "The area of the rectangular "+area);
-        
+
+        System.out.println("Pls enter the value of Radius");
+        int radius = input.nextInt();
+        double area;
+        area = radius * radius * 3.1416;
+        System.out.println(area);
+
+        System.out.println("Please enter height");
+        float height = input.nextFloat();
+        System.out.println("Please enter wedth");
+        float wedht = input.nextFloat();      
+        double rectangul;
+        rectangul = height * wedht;
+        System.out.println("The area of the rectangular " + rectangul);
 
         System.out.print("Please enter num1: ");
         float num1 = input.nextFloat();
         System.out.print("Please enter num2: ");
-          float  num2 = input.nextFloat();
-          
-          System.out.print("Please enter num3: ");
+        float num2 = input.nextFloat();
+
+        System.out.print("Please enter num3: ");
         float num3 = input.nextFloat();
         System.out.print("Please enter num4: ");
-          float  num4 = input.nextFloat();
-          
+        float num4 = input.nextFloat();
+
         float sum;
-        sum = num1+num2+num3+num4;
-        System.out.println( "Total SUM of the Numbers : "+sum);
-                
-        
-        
+        sum = num1 + num2 + num3 + num4;
+        System.out.println("Total SUM of the Numbers : " + sum);
 
     }
-    
+
 }
