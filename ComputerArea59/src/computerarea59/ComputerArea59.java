8d840d3eabfcce7bdba97a19493083554a454745
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-04-22 17:30:12 +0600

    A
    sa

---
+++ ComputerArea59/src/computerarea59/ComputerArea59.java
@@ -0,0 +1,21 @@
+package computerarea59;
+
+
+
+public class ComputerArea59 {
+
+  
+    public static void main(String[] args) {
+       
+        int radius;
+        double area;
+        
+            radius = 10;
+            
+           area = radius*radius*3.1416;
+          
+         System.out.println(area);
+        
+    }
+    
+}
