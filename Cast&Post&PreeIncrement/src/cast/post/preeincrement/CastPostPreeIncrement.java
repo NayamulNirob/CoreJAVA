6547e3c7ffbe1a35300b4481b1285fc31fe7e68c
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-04-24 04:11:58 +0600

    Home
    home

--- Cast&Post&PreeIncrement/src/cast/post/preeincrement/CastPostPreeIncrement.java
+++ Cast&Post&PreeIncrement/src/cast/post/preeincrement/CastPostPreeIncrement.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication1;
 
-/**
- *
- * @author Administrator
- */
-public class JavaApplication1 {
+package cast.post.preeincrement;
 
-    /**
-     * @param args the command line arguments
-     */
+public class CastPostPreeIncrement {
+
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
         
         System.out.println("Hello");
         
