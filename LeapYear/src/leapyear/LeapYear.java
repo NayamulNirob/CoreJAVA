af0ee1c2dbd7ce009bf9ef77c125ddaf394165bb
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-04-24 17:56:25 +0600

    24.04.24
    24.04.24

--- LeapYear/src/leapyear/LeapYear.java
+++ LeapYear/src/leapyear/LeapYear.java
@@ -1,29 +1,32 @@
-
 package leapyear;
 
 import java.util.Scanner;
 
 public class LeapYear {
 
-    
     public static void main(String[] args) {
-      
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Enter a year");
-        
-        int year = input.nextInt();
-        
-        boolean isLeapYear = (year%4==0 && year%100!=0)||(year%400==0);
-        
-        System.out.println(year+"Is a leapyear "+isLeapYear);
-        
-        
-        
-        
-        
-    
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter a year");
+//
+//        int year = input.nextInt();
+//
+//        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+//
+//        System.out.println(year + "Is a leapyear " + isLeapYear);
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please enter year number");
+        int year=scanner.nextInt();
+       
+        if(year%4==0 && year%400==0 && year%100!=0){
+            System.out.println("this year is a Leap Year");
+        }
+        else{
+            System.out.println("This year is not a Leap Year");
+        }
 
     }
-    
+
 }
