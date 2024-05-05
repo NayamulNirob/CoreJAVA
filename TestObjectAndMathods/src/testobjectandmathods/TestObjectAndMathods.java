6dbcf76f41351b144a11cffc256e33a24cd8dc44
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-05-05 18:36:16 +0600

    Add
    add

---
+++ TestObjectAndMathods/src/testobjectandmathods/TestObjectAndMathods.java
@@ -0,0 +1,18 @@
+
+package testobjectandmathods;
+
+public class TestObjectAndMathods {
+
+  
+   
+    public static void main(String[] args) {
+      
+        TestPrimeNumber number= new TestPrimeNumber();
+
+      
+        System.out.println(number.primeNumber());
+    }
+
+  
+   
+}
