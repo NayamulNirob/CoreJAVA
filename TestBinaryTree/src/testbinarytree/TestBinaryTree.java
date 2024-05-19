10711c3c67efa21f1cc5adc8828d1be27bcf3a3f
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-05-19 12:47:29 +0600

    Add
    add

---
+++ TestBinaryTree/src/testbinarytree/TestBinaryTree.java
@@ -0,0 +1,18 @@
+
+package testbinarytree;
+
+
+public class TestBinaryTree {
+
+   
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(60);
+        root.left = new TreeNode<>(55);
+        root.right = new TreeNode<>(100);
+        System.out.println(root);
+        
+    }
+    
+  
+    
+}
---
+++ TestBinaryTree/src/testbinarytree/TreeNode.java
@@ -0,0 +1,32 @@
+
+package testbinarytree;
+
+import javax.swing.plaf.RootPaneUI;
+
+public class TreeNode<E> {
+    
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(E e) {
+        element =e;
+    }
+
+    public TreeNode(E element, TreeNode<E> left, TreeNode<E> right) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" + "element=" + element + ", left=" + left + ", right=" + right + '}';
+    }
+    
+   
+    
+}
