cd5ce53ee408e62bfd3be4bc9f10db274783684c
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-05-28 11:20:06 +0600

    Update DbUtill.java

--- Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
+++ Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
@@ -6,8 +6,8 @@ import java.sql.Connection;
 
 public class DbUtill {
   private Connection con =null;
-  private String url="";
-  private String user="";
-  private String password="";
-  private String driver="";
+  private String url="jdbc:mysql://localhost:3306/testjdbc59";
+  private String user="root";
+  private String password="1234";
+  private String driver="com.mysql.cj.jdbc.Driver";
 }
