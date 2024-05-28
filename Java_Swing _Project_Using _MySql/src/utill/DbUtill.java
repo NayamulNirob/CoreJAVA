c25f16f2448c3ebbd3dc00eec4cf5e9013d63d52
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-05-28 11:37:41 +0600

    Update DbUtill.java

--- Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
+++ Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
@@ -2,12 +2,27 @@
 package utill;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class DbUtill {
   private Connection con =null;
-  private String url="jdbc:mysql://localhost:3306/testjdbc59";
+  private String url="jdbc:mysql://localhost:3306/jee_59_store";
   private String user="root";
-  private String password="1234";
+  private String password="2020";
   private String driver="com.mysql.cj.jdbc.Driver";
+  
+  
+  public Connection getCon() throws ClassNotFoundException, SQLException{
+  
+  Class.forName(driver);
+  con = DriverManager.getConnection(url, user, password);
+  
+  return con;
+  
+  }
+  
+  
+  
 }
