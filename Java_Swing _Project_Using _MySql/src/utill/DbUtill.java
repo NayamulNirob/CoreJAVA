5dfc231bda57dc233f6f9c788430301949b8d16b
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-05-28 10:53:23 +0600

    Update DbUtill.java

--- Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
+++ Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package utill;
 
-/**
- *
- * @author nayam
- */
+import java.sql.Connection;
+
+
 public class DbUtill {
-    
+  private Connection con =null;
+  private String url="";
+  private String user="";
+  private String password="";
+  private String driver="";
 }
