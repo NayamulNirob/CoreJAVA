e5bd0cb894d566bcc7ab405fe3a33d021109538b
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-05-29 16:29:38 +0600

    fainal
    After deleting

---
+++ Java_Swing _Project_Using _MySql/Java_Swing _Project_Using _MySql/src/utill/DbUtill.java
@@ -0,0 +1,28 @@
+
+package utill;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbUtill {
+  private Connection con =null;
+  private String url="jdbc:mysql://localhost:3306/jee59store";
+  private String user="root";
+  private String password="2020";
+  private String driver="com.mysql.cj.jdbc.Driver";
+  
+  
+  public Connection getCon() throws ClassNotFoundException, SQLException{
+  
+  Class.forName(driver);
+  con = DriverManager.getConnection(url, user, password);
+  
+  return con;
+  
+  }
+  
+  
+  
+}
