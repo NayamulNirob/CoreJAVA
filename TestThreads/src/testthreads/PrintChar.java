bc8e5783a7efe0041d8a2a7cd2df069655b98820
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-06-04 10:29:44 +0600

    10:29AM
    04/06/2024

---
+++ TestThreads/src/testthreads/PrintChar.java
@@ -0,0 +1,25 @@
+
+package testthreads;
+
+public class PrintChar implements Runnable{
+
+    private char charToPrint;
+    private int times;
+
+    public PrintChar(char charToPrint, int times) {
+        this.charToPrint = charToPrint;
+        this.times = times;
+    }
+
+    @Override
+    public void run() {
+         for (int i = 0; i < times; i++) {
+             System.out.print(charToPrint);
+        }
+    }
+
+  
+    
+    
+   
+}
