1b8e70abb31925f2da82bae9c53b6defa761ad8f
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-12-24 01:21:08 +0600

    test

--- StartAgian/src/startagian/StartAgian.java
+++ StartAgian/src/startagian/StartAgian.java
@@ -1,6 +1,8 @@
 
 package startagian;
 
+import java.util.Scanner;
+
 public class StartAgian {
 
     public static void main(String[] args) {
@@ -10,6 +12,27 @@ public class StartAgian {
         for (int i = 1; i <5; i++) {
             System.out.println("*".repeat(i));
         }
+        
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Number 1");
+        int n=sc.nextInt();
+        System.out.println("enter 2nd number ");
+        int m=sc.nextInt();
+       
+        if(n==m){
+            System.out.println(n+" equal "+m);
+        }
+        else if(n>m){
+            System.out.println(n+" n is gretter m "+m);
+        }
+        else if(n<m){
+            System.out.println(n+" n is lesser m "+m);
+        }
+        else{
+            System.out.println(n+"  "+m);
+        }
     }
+       
     
 }
--- startAgin2/class Next.java
+++ startAgin2/class Next.java
@@ -1,9 +1,22 @@
-class Next{
-    public static void main(String[] args){
+
+import java.util.Scanner;
+
+class Next {
+
+    public static void main(String[] args) {
         System.out.println("Hello World");
 
         for (int i = 0; i < 10; i++) {
             System.out.println("*".repeat(i));
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
         }
     }
 }
