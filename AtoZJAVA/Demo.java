4be0b9c0a1295a09fccd41a98596d384eaae42d3
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-12-27 02:58:54 +0600

    new

---
+++ AtoZJAVA/Demo.java
@@ -0,0 +1,10 @@
+class Demo {
+    public static void main(String[] args) {
+        double a=5.5;
+        double b=5.6;
+
+        boolean result= a==b;
+
+        System.out.println("Result is: "+result);
+    }
+}
--- AtoZJAVA/Hello.java
+++ AtoZJAVA/Hello.java
@@ -1,5 +1,27 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a=5;
+        int b=6;
+        System.out.println("Sum of a and b is: "+(a+b));
+
+        char c='A';
+        var h= c++;
+        System.out.println("Next character of A is: "+h);
+
+        byte d=127;
+
+        int e=257;
+
+         d=(byte)e; 
+
+        System.out.println("Value of d is: "+d);
+
+        double f=6.14565552;
+        double g=2.555;
+
+        double result=(f/g);
+
+        System.out.println("Result is: "+result);
+        
     }
 }
