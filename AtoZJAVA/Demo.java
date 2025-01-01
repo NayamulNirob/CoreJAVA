bae4aea4bd80d4dc29f1b044abbe92c3f73d7365
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2025-01-02 02:05:55 +0600

    prctice
    2:05AM

--- AtoZJAVA/Demo.java
+++ AtoZJAVA/Demo.java
@@ -32,18 +32,63 @@ class Demo {
         //         ("Invalid Day");
         // };
         // System.out.println("Day is: " + day);
-
-
         // for (char i = 'A'; i <= 'Z'; i++) {
-
         //     System.out.println(i);
         // }
+        // int width = 80; // Terminal width
+        // for (int i = 1; i <= 10; i++) {
+        //     int padding = (width - i) / 2;
+        //     System.out.println(" ".repeat(padding) + "*".repeat(i));
+        // }
+        // int e= 1;
+        // while (e<=10) {
+        //     System.out.println("Hi"+e);
+        //     e++;
+        // }
+        // System.out.println("End of the loop "+e);
+        // do{
+        //     System.out.println("Hello");
+        // }while(false);
+        // int x=5;
+        // int y=10;
+        // System.out.println("Addition is: "+(x*y+y/x));
 
-        int width = 80; // Terminal width
-        for (int i = 1; i <= 10; i++) {
-            int padding = (width - i) / 2;
-            System.out.println(" ".repeat(padding) + "*".repeat(i));
-        }
+
+
+        // for (int i = 1; i <= 5; i++) {
+
+        //     System.out.println("DAY " + i);
+        //     for (int j = 1; j <= 9; j++) {
+        //         if ((j + 8) >= 13 || (j + 9) >= 13) {
+        //             System.out.println(((j + 8) - 12) + " _ " + ((j + 9) - 12));
+        //         } else {
+        //             System.out.println((j + 8) + "_" + (j + 9));
+        //         }
+
+        //     }
+
+        // }
+
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println("DAY " + i);
+        //     for (int j = 1; j <= 9; j++) {
+        //         int startHour = (j + 8) % 12;
+        //         if (startHour == 0) {
+        //             startHour = 12; // 12 hour clock
+        //         }
+        //         int endHour = (j + 9) % 12;
+        //         if (endHour == 0) {
+        //             endHour = 12;
+        //         }
+
+        //         String startPeriod = (j + 8) < 12 || (j + 8) >= 24 ? "AM" : "PM";
+        //         String endPeriod = (j + 9) < 12 || (j + 9) >= 24 ? "AM" : "PM";
+
+        //         System.out.println(startHour + " " + startPeriod + " - " + endHour + " " + endPeriod);
+        //     }
+        // }
+
+        
 
     }
 }
