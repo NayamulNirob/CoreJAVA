f117f3cecc8d0d82ec21b68605ab59c856ee9b43
NayamulNirob <nayamulislam@gmail.com>
2025-01-25 14:01:24 +0600

    Threads breack

--- AtoZJAVA/threds/Threds.java
+++ AtoZJAVA/threds/Threds.java
@@ -1,12 +1,18 @@
-class Threds{
 
-public static void main(String[] args) {
-    A a =new A();
-    B b =new B();
+class Threds {
 
-    a.start();
-    b.start();
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
 
-}
+        a.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        b.start();
+
+    }
 
 }
