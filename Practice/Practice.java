d70f67837db4e0f35cbc908bbb74eca4aa5d4f3b
NayamulNirob <nayamulislam@gmail.com>
2025-01-26 19:50:26 +0600

    hg

---
+++ Practice/Practice.java
@@ -0,0 +1,9 @@
+class Practice{
+    public static void main(String[] args){
+        
+         int crr=3126;
+        int born=crr-8;
+        
+        System.out.println(String.valueOf(born));
+    }
+}
