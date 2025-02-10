16472bacbd6f139064ebf93c5e93b8b2246e6816
NayamulNirob <nayamulislam@gmail.com>
2025-02-10 23:16:34 +0600

    bubbleSort

---
+++ AtoZjavaPractice/src/atozjavapractice/SelectionSort.java
@@ -0,0 +1,45 @@
+
+package atozjavapractice;
+
+
+public class SelectionSort {
+    
+//    public static void main(String[] args) {
+//        
+//        int numbs[]={6,5,2,8,9,4};
+//        int temp=0;
+//        int size=numbs.length;
+//        
+//        System.out.println("Before sorting---");
+//        for(int num:numbs){
+//            System.out.print(" "+num);
+//        }
+//        
+//        for(int i=0; i<size;i++){
+//        
+//            for(int j=0; j<size-i-1;j++){
+//            
+//                if(numbs[j]>numbs[j+1]){
+//                    
+//                    temp=numbs[j];
+//                    numbs[j]=numbs[j+1];
+//                    numbs[j+1]=temp;
+//                }
+//            }
+//            
+//            //To see the sorting 
+//            System.out.println();
+//            for(int num:numbs){
+//                System.out.print(" "+num);
+//            }
+//        }
+//        System.out.println();
+//        
+//         System.out.println("\n After sorting---");
+//        for(int num:numbs){
+//            System.out.print(" "+num);
+//        }
+//        
+//    }
+    
+}
