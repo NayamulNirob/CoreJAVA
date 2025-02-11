cae07a3f33b166496bc989ef0ad37acba4d6cdda
NayamulNirob <nayamulislam@gmail.com>
2025-02-11 10:36:21 +0600

    Inserction sort

--- AtoZjavaPractice/src/atozjavapractice/SelectionSort.java
+++ AtoZjavaPractice/src/atozjavapractice/SelectionSort.java
@@ -2,46 +2,46 @@ package atozjavapractice;
 
 public class SelectionSort {
 
-    public static void main(String[] args) {
-
-        int numbs[] = {6, 5, 2, 8, 9, 4};
-        int temp = 0;
-        int size = numbs.length;
-        int minIndex = -1;
-
-        System.out.println("Before sorting---");
-        for (int num : numbs) {
-            System.out.print(" " + num);
-        }
-
-        for (int i = 0; i < size-1; i++) {
-
-            minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-
-                if (numbs[minIndex] > numbs[j]) {
-
-                    minIndex = j;
-                }
-            }
-
-            temp = numbs[minIndex];
-            numbs[minIndex] = numbs[i];
-            numbs[i] = temp;
-
-            System.out.println();
-            for (int num : numbs) {
-                System.out.print(" " + num);
-            }
-        }
-
-        System.out.println();
-
-        System.out.println("\n After sorting---");
-        for (int num : numbs) {
-            System.out.print(" " + num);
-        }
-
-    }
+//    public static void main(String[] args) {
+//
+//        int numbs[] = {6, 5, 2, 8, 9, 4};
+//        int temp = 0;
+//        int size = numbs.length;
+//        int minIndex = -1;
+//
+//        System.out.println("Before sorting---");
+//        for (int num : numbs) {
+//            System.out.print(" " + num);
+//        }
+//
+//        for (int i = 0; i < size-1; i++) {
+//
+//            minIndex = i;
+//            for (int j = i + 1; j < size; j++) {
+//
+//                if (numbs[minIndex] > numbs[j]) {
+//
+//                    minIndex = j;
+//                }
+//            }
+//
+//            temp = numbs[minIndex];
+//            numbs[minIndex] = numbs[i];
+//            numbs[i] = temp;
+//
+//            System.out.println();
+//            for (int num : numbs) {
+//                System.out.print(" " + num);
+//            }
+//        }
+//
+//        System.out.println();
+//
+//        System.out.println("\n After sorting---");
+//        for (int num : numbs) {
+//            System.out.print(" " + num);
+//        }
+//
+//    }
 
 }
