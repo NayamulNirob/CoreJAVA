193a990c44926c9498053aa6345b8c436813bd5d
NayamulNirob <nayamulislam@gmail.com>
2025-04-19 01:12:44 +0600

    practice

---
+++ Practice.java
@@ -0,0 +1,13 @@
+class Pratice{
+    public static void main(String[] args) {
+      
+        int a = 5;
+
+        System.out.println("Post Increment: " + a++); // 5
+        System.out.println("After Post Increment: " + a); // 6
+
+        int b = 5;
+        System.out.println("Pre Increment: " + ++b); // 6
+        System.out.println("After Pre Increment: " + b); // 6
+    }
+}
