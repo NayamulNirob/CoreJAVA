086463e61d52322a45e171f32b4409b61c5f6f69
NayamulNirob <nayamulislam@gmail.com>
2025-06-15 00:35:15 +0600

    Update AddTenNumberInArray.java

--- AddTenNumberInArray/src/addtennumberinarray/AddTenNumberInArray.java
+++ AddTenNumberInArray/src/addtennumberinarray/AddTenNumberInArray.java
@@ -9,7 +9,7 @@ public class AddTenNumberInArray {
     public static void main(String[] args) {
         
        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[100];
+        int[] numbers = new int[10];
 
         for (int i = 0; i < numbers.length-1; i++) {
             System.out.print(i+" Enter a number: ");
