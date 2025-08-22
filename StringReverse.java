85a9028fd074c21863fbc7e528198bd46f531406
NayamulNirob <nayamulislam@gmail.com>
2025-08-23 02:23:10 +0600

    String reverse

--- StringReverse.java
+++ StringReverse.java
@@ -8,6 +8,24 @@ public class StringReverse {
         System.out.println("Reversed string: " + reversed);
 
         System.out.println(revStr("Hello"));
+
+        // String reversed using for loop
+        String str = "Java";
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + reverse);
+
+
+        //String reversed using for loop & charArray
+        String stri = "OpenAI";
+        char[] ch = stri.toCharArray();
+        for (int i = ch.length - 1; i >= 0; i--) {
+            System.out.print(ch[i]);
+        }
     }
 
        public static String revStr(String str) {
